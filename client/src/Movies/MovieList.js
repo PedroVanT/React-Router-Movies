825448f727c410f14b6dd04b733c2971213242f7
Pedro Van Thienen <pedrovanthienen@gmail.com>
2020-10-12 22:21:55 -0400

    added alternative to MovieList

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -33,3 +33,32 @@ function MovieDetails(props) {
     </div>
   );
 }
+
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// export default function MovieList(props) {
+//   const {movies} = props
+//   return (
+//     <div className="movie-list">
+//       {movies.map(movie => (
+//         <Link to={`/movie/${movie.id}`}>
+//         <MovieDetails key={movie.id} movie={movie} />
+//         </Link>
+//       ))}
+//     </div>
+//   );
+// }
+// function MovieDetails(props) {
+//   const { title, director, metascore } = props.movie;
+//   return (
+//     <div className="movie-card">
+//       <h2>{title}</h2>
+//       <div className="movie-director">
+//         Director: <em>{director}</em>
+//       </div>
+//       <div className="movie-metascore">
+//         Metascore: <strong>{metascore}</strong>
+//       </div>
+//     </div>
+//   );
+// }
